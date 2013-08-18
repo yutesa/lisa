fa2e4ee8b882759d8cdb8278c31ff6d921140697
monchoz <ramonzuniga9@gmail.com>
2013-08-18 00:43:12 -0600

    Agregue nuevo logo, cambios en footer y estilo de main page.

--- lisa/settings.py
+++ lisa/settings.py
@@ -16,8 +16,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lisa_estore',                      # Or path to database file if using sqlite3.
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '',
